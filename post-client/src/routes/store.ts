e567b786f31f63ee87b049b7b4cb16e1cf632bfa
Michael <mstephenp@protonmail.com>
2021-11-16 04:27:45 -0600

    removing sveltekit app

--- post-client/src/routes/store.ts
+++
@@ -1,7 +0,0 @@
-import { writable, derived } from 'svelte/store';
-
-export const postData = writable([]);
-
-export const posts = derived(postData, ($postData) => {
-    return $postData.map(post => post.content)
-});
